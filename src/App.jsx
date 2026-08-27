1a47712ddb88fde1b848d2f5fc8538482c8fcd04
skyvaultex <maxym.vorotnik@gmail.com>
2026-08-26 20:46:03 -0500

    Route main page and checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { HomePage } from './pages/HomePage'
+import Checkout from './pages/Checkout'
+import { Routes, Route } from 'react-router'
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<Checkout />} />
+    </Routes>
   )
 }
 
