5c675cfd31159e23ef2b30f1756a79501ab01277
skyvaultex <maxym.vorotnik@gmail.com>
2026-08-29 14:49:08 -0500

    Move Order and Tracking Pages to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HomePage } from './pages/HomePage'
 import Checkout from './pages/Checkout'
 import Tracking from './pages/Tracking'
+import Orders from './pages/Orders'
 import { Routes, Route } from 'react-router'
 import './App.css'
 
@@ -10,6 +11,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<Checkout />} />
       <Route path="tracking" element={<Tracking />} />
+      <Route path="orders" element={<Orders />} />
     </Routes>
   )
 }
--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -1,3 +1,5 @@
+import './Tracking.css'
+
 function Tracking() {
   return (
     <>
